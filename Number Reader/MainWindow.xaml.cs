275a5109dcb7904e0e0b82d9e6cc731c8e11a87e
Malosdaf <bellmang2005@gmail.com>
2022-05-20 22:47:20 +0700

    Upload code

---
+++ Number Reader/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Number_Reader
+{
+    public enum UnitEnglish
+    {
+        Hundred,
+        Thousand,
+        Million,
+        Billion,
+        Trillion,
+        Quadrillion,
+        Quintillion
+    }
+
+    public enum Lang
+    {
+        Vietnamese,
+        English
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private EnglishReader englishReader = new EnglishReader();
+        private VietnameseReader vietnameseReader = new VietnameseReader();
+        private Lang CurrentLanguage = Lang.English;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HandleNumericInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int x);
+        }
+
+        private void TranslateToEnglish_CheckedChanged(object sender, EventArgs e)
+        {
+            CurrentLanguage = Lang.English;
+        }
+
+        private void TranslateToVietnamese_CheckedChanged(object sender, EventArgs e)
+        {
+            CurrentLanguage = Lang.Vietnamese;
+        }
+
+        private void Translate_Click(object sender, EventArgs e)
+        {
+            switch (CurrentLanguage)
+            {
+                case Lang.English:
+                    Translate_English(TbInput.Text);
+                    return;
+                case Lang.Vietnamese:
+                    Translate_Vietnamese(TbInput.Text);
+                    return;
+            }
+        }
+
+        private void Translate_English(string number)
+        {
+            TbOutput.Text = englishReader.Read(number);
+        }
+
+        private void Translate_Vietnamese(string number)
+        {
+            // change number to spelling
+            TbOutput.Text = vietnameseReader.Read(number);
+        }
+
+        private void TbInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextToSpeak(object sender, EventArgs e)
+        {
+            Translate_Click(this, null);
+
+            string speech = TbOutput.Text;
+            switch (CurrentLanguage)
+            {
+                case Lang.English:
+                    {
+                        Thread t = new Thread(new ThreadStart(() =>
+                        {
+                            using (SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer())
+                            {
+                                speechSynthesizer.Speak(speech);
+                            }
+                        }));
+                        t.IsBackground = true;
+                        t.Start();
+                    }
+                    return;
+                case Lang.Vietnamese:
+                    {
+                        Thread t = new Thread(new ThreadStart(() => PlaySoundVietnamese(speech)));
+                        t.IsBackground = true;
+                        t.Start();
+                    }
+                    return;
+            }
+        }
+
+        private void PlaySoundVietnamese(string numberText)
+        {
+            string[] numbers = numberText
+                .Split(' ')
+                .Select(x => x.ToLower())
+                .ToArray();
+
+            foreach (string num in numbers)
+            {
+                if (string.IsNullOrEmpty(num))
+                {
+                    continue;
+                }
+
+                SoundPlayer player = new SoundPlayer($"Sound/{num}Male.wav");
+                player.PlaySync();
+            }
+        }
+
+
+    }
+}
---
+++ Number Reader/Reader.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Reader
+{
+    public class EnglishReader
+    {
+        private readonly string[] number = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        private readonly string[] teen_num = { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        private readonly string[] ten = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+        private readonly string[] level = { string.Empty, "thousand", "million", "billion", "trillion", "quadrillion", "quintillions" };
+
+        private string Hundred(char s)
+        {
+            if (s == '0')
+            {
+                return string.Empty;
+            }
+            return $"{number[s - '0']}-hundred";
+        }
+
+        public string LastTwoNumbers(string s)
+        {
+            if (s[0] == '0')
+            {
+                if (s[1] == '0')
+                {
+                    return string.Empty;
+                }
+                return number[s[1] - '0'];
+            }
+            else if (s[0] == '1')
+            {
+                return (teen_num[s[1] - '0']);
+            }
+            else
+            {
+                return $"{ten[s[0] - '2']} {number[s[1] - '0']}";
+            }
+        }
+
+        public string ReadThreeNumbers(string s)
+        {
+            string hun = Hundred(s[0]);
+            string t = string.Empty;
+            t += s[1];
+            t += s[2];
+            string lt = LastTwoNumbers(t);
+            if (hun == string.Empty && lt == string.Empty)
+            {
+                return string.Empty;
+            }
+            else if (hun == string.Empty)
+            {
+                return lt;
+            }
+            else if (lt == string.Empty)
+            {
+                return hun;
+            }
+            return $"{hun} and {lt}";
+        }
+
+        public string Read(string s)
+        {
+            int l = 0;
+            string res = string.Empty;
+            for (int i = s.Length - 1; i >= 0; i -= 3)
+            {
+                char a, b, c;
+                a = s[i];
+                if (i - 1 < 0)
+                {
+                    b = '0';
+                }
+                else
+                {
+                    b = s[i - 1];
+                }
+                if (i - 2 < 0)
+                {
+                    c = '0';
+                }
+                else
+                {
+                    c = s[i - 2];
+                }
+                string u = string.Empty;
+                u += c; u += b; u += a;
+                res =
+                    (u == "001" || l == 0) ?
+                    $"{ReadThreeNumbers(u)} {level[l]} {res}" :
+                    $"{ReadThreeNumbers(u)} {level[l]} {res}";
+                l++;
+            }
+            return res;
+        }
+    };
+
+    public class VietnameseReader
+    {
+        private readonly string[] number = { string.Empty, "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
+        private readonly string[] level = { string.Empty, "nghìn", "triệu", "tỉ", "nghìn tỉ", "triệu tỉ" };
+
+        private string Hundred(char s)
+        {
+            if (s == '0')
+            {
+                return "không trăm";
+            }
+            return $"{number[s - '0']} trăm";
+        }
+
+        public string LastTwoNumbers(string s)
+        {
+            if (s[0] == '0')
+            {
+                if (s[1] == '0')
+                {
+                    return string.Empty;
+                }
+                return number[s[1] - '0'];
+            }
+            else if (s[0] == '1')
+            {
+                if (s[1] == '0')
+                {
+                    return "mười";
+                }
+                return $"mười {number[s[1] - '0']}";
+            }
+            else
+            {
+                string res = $"{number[s[0] - '0']} mươi ";
+                if (s[1] == 0)
+                {
+                    return res;
+                }
+                res += number[s[1] - '0'];
+                return res;
+            }
+        }
+
+        public string ReadThreeNumbers(string s)
+        {
+            string hun = Hundred(s[0]);
+            string t = $"{s[1]}{s[2]}";
+            string lt = LastTwoNumbers(t);
+            if (s[1] == '0')
+            {
+                if (s[2] == '0')
+                {
+                    return hun;
+                }
+                return $"{hun} lẻ {lt}";
+            }
+            return $"{hun} {lt}";
+        }
+
+        public string Read(string s)
+        {
+            int l = 0;
+            string res = string.Empty;
+            for (int i = s.Length - 1; i >= 0; i -= 3)
+            {
+                char a, b, c;
+                int cnt = 1;
+                a = s[i];
+                if (i - 1 < 0)
+                {
+                    b = '0';
+
+                }
+                else
+                {
+                    b = s[i - 1];
+                    cnt++;
+                }
+                if (i - 2 < 0)
+                {
+                    c = '0';
+
+                }
+                else
+                {
+                    c = s[i - 2];
+                    cnt++;
+                }
+                string u = $"{c}{b}{a}";
+
+                if (cnt < 3)
+                {
+                    string p = $"{b}{a}";
+                    res = $"{LastTwoNumbers(p)} {level[l]} {res}";
+                }
+                else if (u != "000")
+                {
+                    res = $"{ReadThreeNumbers(u)} {level[l]} {res}";
+                }
+                l++;
+            }
+            return res;
+        }
+    };
+
+}
